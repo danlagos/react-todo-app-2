f40c03459bb6b8db093c43f6e087856d3b099734
Daniel Lagos <lagos.d@gmail.com>
2020-01-15 15:48:57 -0800

    add state for checkbox.

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,32 @@
 class Todo extends React.Component {
-  render(){
 
-    let checked = (this.props.done =="true");
-    
+constructor(props){
+  super(props);
+
+  this.state = { done: (this.props.done == "true" && props.done),
+                  text: props.text 
+                };
+
+  this.handleClick = this.handleClick.bind(this);
+}
+
+  handleClick(event){
+    this.setState(state => ({
+      done: !state.done
+    }));
+  }
+
+  render(){
 
     return <div className="todo">
               <span>
-                <input type="checkbox" checked={checked} /> 
-                <input type="text" value={this.props.text} />
+                <input type="checkbox" checked={this.state.done} onClick={this.handleClick}/> 
+                <input type="text" value={this.state.text}/>
               </span>
             </div>
   }
 }
 
-
 ReactDOM.render(
-  <Todo text="Todo 1" done="false"/>,
+  <Todo text="Todo 1" done="true"/>,
   document.getElementById('root'));
